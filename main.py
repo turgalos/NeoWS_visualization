22e478f726d46e0f2ee3a6e3ea3e59b9883fec9f
mlorek <mlorek@man.poznan.pl>
2024-02-09 15:38:51 +0100

    Migrate to plotly

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import json, os, requests
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from matplotlib.ticker import FuncFormatter
+import plotly.express as px
 from datetime import datetime
 from collections import namedtuple
+import pandas as pd
 
-def km_formatter(x, pos):
-    return f'{int(int(x)/1000000)}'
 
 api_key = os.getenv("NASA_API_KEY")
+start_date = "2024-02-09"
+end_date = "2024-02-11"
 
-start_date = "2024-02-08"
-end_date = "2024-02-09"
-
-url = "https://api.nasa.gov/neo/rest/v1/feed?" \
-    f"start_date={start_date}&end_date={end_date}&api_key={api_key}"
+url = 'https://api.nasa.gov/neo/rest/v1/feed?' \
+      f'start_date={start_date}&end_date={end_date}&api_key={api_key}'
 
 response = requests.get(url)
 data = response.json()
-
 
 with open("asteroids.json", "w") as file:
     json.dump(data, file, indent=4)
@@ -27,46 +22,34 @@ with open("asteroids.json", "r") as file:
     data = json.load(file)
 
 num_of_NEO = data["element_count"]
-i = 0
-# print(json.dumps(data["near_earth_objects"]["2024-02-09"][i], indent=4))
-# print(data["near_earth_objects"]["2024-02-09"][i]["name"])
-# print(data["near_earth_objects"]["2024-02-09"][i]["close_approach_data"][0]["close_approach_date_full"])
-# print(data["near_earth_objects"]["2024-02-09"][i]["close_approach_data"][0]["miss_distance"]["kilometers"])
-
 NEO = namedtuple('NEO', ['date', 'distance', 'name', 'id'])
 neo_list = []
-# new_todos = [item.strip("\n") for item in todos]
-for i in data["near_earth_objects"]["2024-02-09"]: # co z tą datą???
-    print(i["name"])
-    neo_list.append(NEO(
-        date=i["close_approach_data"][0]["close_approach_date_full"],
-        distance=round(float(i["close_approach_data"][0]["miss_distance"]["kilometers"])),
-        name=i["name"],
-        id=i["id"]
+
+for j in data["near_earth_objects"]:
+    for i in data["near_earth_objects"][j]:
+        neo_list.append(NEO(
+            date=i["close_approach_data"][0]["close_approach_date_full"],
+            distance=round(float(i["close_approach_data"][0]["miss_distance"]["kilometers"])),
+            name=i["name"],
+            id=i["id"]
         ))
 
-for neo in neo_list:
-    print(neo)
+# for neo in neo_list:
+#     print(neo)
 
 dates = [datetime.strptime(neo.date, "%Y-%b-%d %H:%M") for neo in neo_list]
 distances = [neo.distance for neo in neo_list]
-# labels = [f"{neo.name[1:-1]}, ({neo.id})" for neo in neo_list]
-labels = [f"id: {neo.id} \nname: {neo.name}" for neo in neo_list]
+labels = [f"id: {neo.id} | name: {neo.name}" for neo in neo_list]
 
-fig, ax = plt.subplots()
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
-ax.xaxis.set_major_locator(mdates.AutoDateLocator())
-ax.yaxis.set_major_formatter(FuncFormatter(km_formatter))
-ax.scatter(dates, distances)
+df = pd.DataFrame({
+    'Date': [neo.date for neo in neo_list],
+    'Distance': [neo.distance for neo in neo_list],
+    'Name & ID': [f"{neo.name} | {neo.id}" for neo in neo_list]
+})
 
-for i, label in enumerate(labels):
-    ax.annotate(label, (dates[i], distances[i]), textcoords="offset points", xytext=(0, 10))
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%b-%d %H:%M')
+fig = px.scatter(df, x='Date', y='Distance', text='Name & ID', title='Obiekty w przestrzeni okołoziemskiej:')
+fig.update_yaxes(tickprefix="", ticksuffix=" km")
+fig.update_traces(mode='markers+text', textposition='top center', marker=dict(size=5), hoverinfo='text')
 
-
-plt.xticks(rotation=45)
-plt.xlabel('Data i godzina zbliżenia')
-plt.ylabel('Odległość [mln km]')
-plt.title('Odległość obiektu od Ziemi')
-plt.tight_layout()
-plt.show()
-
+fig.show()
